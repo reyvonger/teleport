84e43d8992a4d9c57af7fb4a23a5794e944e3296
Guillaume J. Charmes <git+guillaume@charmes.net>
2025-06-24 11:48:37 -0400

    Apply suggestions from code review
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -532,7 +532,7 @@ type CLIConf struct {
 	// kubeAllNamespaces allows users to search for resources in every namespace.
 	kubeAllNamespaces bool
 
-	// kubeAllNamespacesLabel allows to search for resources.
+	// kubeResourceKind allows to search for resources.
 	kubeResourceKind string
 
 	// kubeAPIGroup allows to search for CRD and unknown resources.
@@ -1315,7 +1315,7 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 	reqSearch.Flag("kube-kind", fmt.Sprintf("Kubernetes resource kind name (plural) to search for. Required with --kind=%q Ex: pods, deployements, namespaces, etc.", types.KindKubernetesResource)).StringVar(&cf.kubeResourceKind)
 	reqSearch.Flag("kube-api-group", "Kubernetes API group to search for resources.").StringVar(&cf.kubeAPIGroup)
 	reqSearch.PreAction(func(*kingpin.ParseContext) error {
-		// TODO(@creack): Remove this in v20. Allow legacy kinds with a warning for now.
+		// TODO(@creack): DELETE IN v20.0.0. Allow legacy kinds with a warning for now.
 		if slices.Contains(types.LegacyRequestableKubeResourceKinds, cf.ResourceKind) {
 			fmt.Fprintf(os.Stderr, "Warning: %q is deprecated, use %q instead with --kube-kind and --kube-api-group.\n", cf.ResourceKind, types.KindKubernetesResource)
 			cf.kubeAPIGroup = types.KubernetesResourcesV7KindGroups[cf.ResourceKind]
